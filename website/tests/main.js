d9093d22eed8fc054b0878e3d8947e9bf7aa3cb8
Wilco de Boer <deboer.wilco@gmail.com>
2021-03-05 13:32:49 +0100

    Use different Meteor app base

--- website/tests/main.js
+++ website/tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("FactoryFloor", function () {
+describe("simple-todos", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "FactoryFloor");
+    assert.strictEqual(name, "simple-todos");
   });
 
   if (Meteor.isClient) {
